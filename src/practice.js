0c1745a30a0fb3fcd21ed97b29cef5f80b1a6187
Arjun Nayak <arjun@mavonic.com>
2024-03-28 12:07:05 +0530

    worked on objects and object destructing

--- src/practice.js
+++ src/practice.js
@@ -14,3 +14,17 @@ const subtract = (a, b) => {
 
 console.log(`Addition of 10 and 5 ==> `, add(10, 5));
 console.log(`Subtraction of 10 and 5 ==> `, subtract(10, 5));
+
+const person = {
+  name: "John",
+  age: 30,
+  greet: function () {
+    console.log("Hello, I am " + this.name);
+  },
+};
+
+person.greet();
+
+const { name, age } = person;
+
+console.log(`This is a destructed value =====> Name: ${name}, Age: ${age}`);

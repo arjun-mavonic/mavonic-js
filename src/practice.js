cf656b27e60d1b3a355aa083c866234a727059db
Arjun Nayak <arjun@mavonic.com>
2024-03-28 12:00:24 +0530

    worked on creating lamda function

--- src/practice.js
+++ src/practice.js
@@ -3,3 +3,14 @@ const a = 10;
 const b = `my current value is ${a}`;
 
 console.log(b);
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+console.log(`Addition of 10 and 5 ==> `, add(10, 5));
+console.log(`Subtraction of 10 and 5 ==> `, subtract(10, 5));
